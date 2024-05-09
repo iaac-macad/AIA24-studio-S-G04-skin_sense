8fd505d4f2252a59561cee16298f91b88ab63a4a
chaitali-89 <chaitali.rajendra.phulambrikar@students.iaac.net>
2024-05-10 00:39:10 +0530

    RGB encoding script almost final

---
+++ trial.py
@@ -0,0 +1,9 @@
+# Import standard Libraries
+import pandas as pd
+import seaborn as sns
+import altair as alt
+import matplotlib.pyplot as plt
+
+
+sns.set(rc={'figure.figsize':(10,10)})
+print("imports ok")
